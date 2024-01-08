e8f10e85337a5187c8fd9c96d0f6cc67b5ff86dd
msdpramod <kuppapramod@gmail.com>
2024-01-08 16:19:12 +0530

    defining Path variables in controllers

--- src/main/java/org/commerceproject/ecommerceprodcutservice/Controller/ProductController.java
+++ src/main/java/org/commerceproject/ecommerceprodcutservice/Controller/ProductController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController("/products")
 
 public class ProductController {
-        @GetMapping()
+        @GetMapping(" ")
     public void getAllProducts() {
 
     }
