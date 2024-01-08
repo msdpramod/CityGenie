aa25968ba16e1451411cbd4fe13b2080e63c040a
msdpramod <kuppapramod@gmail.com>
2024-01-08 15:32:30 +0530

    hi welcome

---
+++ src/main/java/org/commerceproject/ecommerceprodcutservice/ECommerceProdcutServiceApplication.java
@@ -0,0 +1,13 @@
+package org.commerceproject.ecommerceprodcutservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceProdcutServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ECommerceProdcutServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/commerceproject/ecommerceprodcutservice/ECommerceProdcutServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.commerceproject.ecommerceprodcutservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceProdcutServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
